ab81babc381b1d25517f70eeb684c525146ed7a9
Jake Johanson <jakegj8683@outlook.com>
2023-11-27 15:44:04 -0800

    challenge template, and page template

--- server/server.js
+++ server/server.js
@@ -15,12 +15,21 @@ var app = express()
 // Set render engine, pug files need to be within views folder
 app.set('view engine', 'pug')
 
+const test_challenge = {
+  name: "Fun Test",
+  points: 100,
+  description: "Testing the pug template for challenges",
+  category: "misc"
+}
 
+const compiledChallengeInfo = pug.compileFile('./views/challenge-info.pug');
 
 app.get('/', function(req, res) {
   res.render('index')
 })
-
+app.get('/challenge/1', function(req, res) {
+  res.send(compiledChallengeInfo(test_challenge))
+})
 // Express static serving
 app.use('/', express.static('static'))
 
