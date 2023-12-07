33dba33ebd48b14ed450a15cd757be0a768cc39a
ashermorgan <59518073+ashermorgan@users.noreply.github.com>
2023-12-06 16:25:28 -0800

    Revert "Implement submit_flag() and add chal.team_solved"

--- models/init.sql
+++ models/init.sql
@@ -1,7 +1,4 @@
-DROP FUNCTION IF EXISTS my_schema.get_category;
-DROP FUNCTION IF EXISTS my_schema.get_challenge;
 DROP PROCEDURE IF EXISTS my_schema.add_solve;
-DROP FUNCTION IF EXISTS my_schema.submit_flag;
 DROP TABLE IF EXISTS my_schema.solves;
 DROP TABLE IF EXISTS my_schema.challenges;
 DROP TABLE IF EXISTS my_schema.teams;
@@ -37,26 +34,6 @@ CREATE TABLE my_schema.solves (
     PRIMARY KEY (team_id, challenge_id)
 );
 
-CREATE FUNCTION my_schema.get_category(_category categories, _team INTEGER = -1)
-    RETURNS TABLE(id INTEGER, name varchar(64), points INTEGER, description varchar(1024), flag varchar(64), solves INTEGER, team_solved BOOLEAN)
-LANGUAGE SQL
-AS $$
-    SELECT id,name,points,description,flag,solves,
-        EXISTS (SELECT FROM solves WHERE challenge_id = id AND team_id = _team) AS team_solved
-    FROM challenges
-    WHERE category = _category;
-$$;
-
-CREATE FUNCTION my_schema.get_challenge(_challenge INTEGER, _team INTEGER = -1)
-    RETURNS TABLE(id INTEGER, name varchar(64), category categories, points INTEGER, description varchar(1024), flag varchar(64), solves INTEGER, team_solved BOOLEAN)
-LANGUAGE SQL
-AS $$
-    SELECT id,name,category,points,description,flag,solves,
-        EXISTS (SELECT FROM solves WHERE challenge_id = id AND team_id = _team) AS team_solved
-    FROM challenges
-    WHERE id = _challenge;
-$$;
-
 CREATE PROCEDURE my_schema.add_solve(challenge INTEGER, team INTEGER)
 LANGUAGE SQL
 AS $$
@@ -65,19 +42,4 @@ AS $$
     UPDATE my_schema.teams SET points = points +
         (SELECT points FROM my_schema.challenges WHERE id = challenge)
     WHERE id = team;
-$$;
-
-CREATE FUNCTION my_schema.submit_flag(_challenge INTEGER, _team INTEGER, _flag varchar(64)) RETURNS BOOLEAN
-LANGUAGE PLPGSQL
-AS $$
-BEGIN
-    IF EXISTS(SELECT FROM my_schema.challenges WHERE id = _challenge AND flag = _flag) THEN
-        IF NOT EXISTS(SELECT FROM my_schema.solves WHERE challenge_id = _challenge AND team_id = _team) THEN
-            CALL my_schema.add_solve(_challenge, _team);
-        END IF;
-        RETURN 1;
-    ELSE
-        RETURN 0;
-    END IF;
-END
 $$;
